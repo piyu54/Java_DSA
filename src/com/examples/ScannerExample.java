ac891805a207dcdc2df8ec0a499e1a69b870f773
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:45:32 +0530

    commit

--- src/com/examples/ScannerExample.java
+++ src/com/examples/ScannerExample.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class ScannerExample {
 	public static void main(String[] args) 
     {
+		//here its display an syso line 
+		System.out.println("enter an name");
+		/*here Creates a Scanner object named s.
+		Scanner is used to read input from the keyboard*/
 		Scanner s = new Scanner(System.in);
+		//here nextLine is read an entire line of text
         String name =s.nextLine();
-        System.out.println(name);
+        System.out.println("Your name is "+name);
         s.close();
     }
 }

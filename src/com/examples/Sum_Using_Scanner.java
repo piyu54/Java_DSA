e3c49a238be216206c296e9764f5ffdcecd1a3cb
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:46:04 +0530

    commit

--- src/com/examples/Sum_Using_Scanner.java
+++ src/com/examples/Sum_Using_Scanner.java
@@ -3,6 +3,7 @@ package com.examples;
 import java.util.Scanner;
 
 public class Sum_Using_Scanner {
+	//same as multiplication
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter the first number");

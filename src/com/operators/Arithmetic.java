23f45e72f9f8815420ea50b86aa2453013ecfda6
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 17:04:27 +0530

    commit

--- src/com/operators/Arithmetic.java
+++ src/com/operators/Arithmetic.java
@@ -3,6 +3,7 @@ package com.operators;
 public class Arithmetic {
 
 	public static void main(String[] args) {
+		//Arithmetic operators 
 		int a = 10;
 		int b = 20;
 		System.out.println("add is :" + (a + b));

82451922ea9f3a92151fe6ed4b5c36a59ea10df8
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:20:22 +0530

    commit

---
+++ src/com/examples/Calculator.java
@@ -0,0 +1,40 @@
+package com.examples;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner sr = new Scanner(System.in);
+        System.out.println("enter your first number");
+        int num=sr.nextInt();
+        System.out.println("enter your second number");
+        int num2=sr.nextInt();
+        System.out.println("enter your sign +,-,*,/,% ");
+       char operator =sr.next().charAt(0);
+        
+        switch (operator) {
+            case '+':System.out.println("addition of two num is "+(num+num2));
+                
+            break;
+             case '-':System.out.println("SUB of two num " +(num-num2));
+                
+            break;
+            case '*':System.out.println("MULTI of two num "+(num*num2));
+                
+            break;
+            case '/':System.out.println("DIVISION of two num "+(num/num2));
+                
+            break;
+            case '%':System.out.println("MODULOR of two num "+(num%num2));
+                
+            break;
+            default:System.out.println("you Put the incorrect input");
+                break;
+        }
+        sr.close();
+    }
+
+	}
+
+

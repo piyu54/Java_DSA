5d3b80ddb1d911e858479f520c21ab77798a0dc1
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:24:04 +0530

    commit

---
+++ src/com/examples/Areaofcircle.java
@@ -0,0 +1,15 @@
+package com.examples;
+
+import java.util.Scanner;
+
+public class Areaofcircle {
+	public static void main(String[] args) {
+
+		Scanner sr = new Scanner(System.in);
+		System.out.println("enter your radius");
+		float radius = sr.nextFloat();
+		float area = 3.14f * radius * radius;
+		System.out.println("Area of the circle is " + area);
+		sr.close();
+	}
+}

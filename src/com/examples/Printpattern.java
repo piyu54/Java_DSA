6a964b361a065300c7a98c6f0568be79a43d4899
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:23:52 +0530

    commit

---
+++ src/com/examples/Printpattern.java
@@ -0,0 +1,13 @@
+package com.examples;
+
+public class Printpattern {
+	public static void main(String arg[])
+
+	{
+
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+	}
+}

ae5c8a40e5f024c68d93e4b4b067da186500ea0a
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:42:18 +0530

    commit

--- src/com/examples/Product.java
+++ src/com/examples/Product.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Product {
 	public static void main(String[] args) {
-
+//multiplication using scanner
 		Scanner sr = new Scanner(System.in);
-		System.out.println("enter your first number");
+		System.out.println("enter your first number");//first num
 		int a = sr.nextInt();
-		System.out.println("enter your second number");
+		System.out.println("enter your second number");//second num
 		int b = sr.nextInt();
-		System.out.print("multi of two number");
+		System.out.print("multi of two number "); //operation
 		int product = a * b;
 		System.out.println(product);
 		sr.close();

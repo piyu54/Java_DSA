ce67b5f93dcde11e4c3d5e3f641fc3322bb502f6
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:35:09 +0530

    commit

--- src/com/examples/Areaofcircle.java
+++ src/com/examples/Areaofcircle.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Areaofcircle {
 	public static void main(String[] args) {
-
+		//scanner is used to take an input from user
 		Scanner sr = new Scanner(System.in);
 		System.out.println("enter your radius");
 		float radius = sr.nextFloat();

ce1a652459be1733287e3110e9cf5b0b1cc0d332
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 17:11:43 +0530

    commit

--- src/com/operators/Logical.java
+++ src/com/operators/Logical.java
@@ -3,6 +3,21 @@ package com.operators;
 public class Logical {
 
 	public static void main(String[] args) {
+		/*logical operators 
+		 * AND T T = T
+		 * 	   T F = F
+		 * 	   F T = F
+		 *     F F = F
+		 *  OR T T = T
+		 * 	   T F = T
+		 * 	   F T = T
+		 *     F F = F
+		 *     
+		 *  NOT
+		 *  F=T
+		 *  T=F
+		 * 	*/
+		
 		int a = 16;
 		int b = 15;
 		System.out.println((a > b && b < a));

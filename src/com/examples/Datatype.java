2682b65ff280c3ec5e375068ae0b305b0136e73a
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:23:59 +0530

    commit

---
+++ src/com/examples/Datatype.java
@@ -0,0 +1,24 @@
+package com.examples;
+
+public class Datatype {
+	public static void main(String[] args) 
+    {
+        byte b=8;
+        System.out.println(b);
+        short s=5;
+        System.out.println(s);
+        char c='a';
+        System.out.println(c);
+        boolean bo=true;
+        System.out.println(bo);
+        int p=10;
+        System.out.println(p);
+        long l=1355;
+        System.out.println(l);
+        float f=334f;
+        System.out.println(f);
+        double d=5656.7;
+        System.out.println(d);
+    }
+
+}

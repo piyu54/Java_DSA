2b3da332e4905b0f73c322551ea70ada53c5ad99
piyu54 <priyakushwaha1407@gmail.com>
2025-11-14 16:41:01 +0530

    commit

--- src/com/examples/Printpattern.java
+++ src/com/examples/Printpattern.java
@@ -4,7 +4,7 @@ public class Printpattern {
 	public static void main(String arg[])
 
 	{
-
+		//this is used to print syso lines
 		System.out.println("****");
 		System.out.println("***");
 		System.out.println("**");

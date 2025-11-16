048e720bef4fc593f2d5ab3ef2027d5a59400eb0
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 00:05:10 +0530

    commit

--- src/com/operatorsQuestions/Example8.java
+++ src/com/operatorsQuestions/Example8.java
@@ -5,7 +5,7 @@ public class Example8 {
 	public static void main(String[] args) {
 
 		int x = 5, y = 10;
-		System.out.println(x > y ? x : y);
+		System.out.println(x > y ? x : y);//5>10 false means 10 
 
 	}
 

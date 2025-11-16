5feb019724e829b071acb97a507435837a5e538b
piyu54 <priyakushwaha1407@gmail.com>
2025-11-16 23:11:17 +0530

    commit

--- src/com/conditionalstatement/Ifelse.java
+++ src/com/conditionalstatement/Ifelse.java
@@ -3,6 +3,7 @@ package com.conditionalstatement;
 public class Ifelse {
 	public static void main(String[] args) {
 
+		//if else
 		int age = 17;
 		if (age >= 18) {
 			System.out.println("he or she is adult");

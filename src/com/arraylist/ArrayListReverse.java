d3591afbd6bd61bc14731f41f4d3ac65a1d66f2c
asus <priyakushwaha1407@gmail.com>
2026-01-20 17:21:27 +0530

    commit

---
+++ src/com/arraylist/ArrayListReverse.java
@@ -0,0 +1,22 @@
+package com.arraylist;
+import java.util.ArrayList;
+public class ArrayListReverse {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> l=new ArrayList<Integer>();
+		l.add(10);
+		l.add(30);
+		l.add(40);
+		l.add(50);
+		l.add(60);
+		l.add(70);
+		
+		for(int i=l.size()-1;i>=0;i--)
+		{
+			System.out.print(l.get(i)+" ");
+		}
+		System.out.println();
+	}
+	
+}

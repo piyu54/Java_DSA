4d1affe45c6c8d422ca2314c24690f8dcf8b1534
asus <priyakushwaha1407@gmail.com>
2026-01-20 17:21:44 +0530

    commit

---
+++ src/com/arraylist/SwapArrayList.java
@@ -0,0 +1,33 @@
+package com.arraylist;
+
+import java.util.ArrayList;
+
+public class SwapArrayList {
+
+	
+	public static void swap(ArrayList< Integer> l,int index1,int index2)
+	{
+		int temp=l.get(index1);
+		l.set(index1,l.get(index2));
+		l.set(index2,temp);
+	}
+	public static void main(String[] args) {
+		
+		ArrayList< Integer> l = new ArrayList<Integer>();
+		l.add(10);
+		l.add(30);
+		
+		
+		
+		int index1=0;
+		int index2=1;
+		//before
+		System.out.println(l);
+		swap(l, index1, index2);
+		//after
+		System.out.println(l);
+		
+		
+
+	}
+}

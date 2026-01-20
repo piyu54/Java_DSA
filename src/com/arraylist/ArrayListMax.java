a0650766d5aeac8409860cdfbcf1e2cee9367f7a
asus <priyakushwaha1407@gmail.com>
2026-01-20 16:06:27 +0530

    commit

---
+++ src/com/arraylist/ArrayListMax.java
@@ -0,0 +1,28 @@
+package com.arraylist;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class ArrayListMax{
+
+	public static void main(String[] args) {
+
+		int max=0;
+		ArrayList< Integer> l = new ArrayList<Integer>();
+		l.add(10);
+		l.add(30);
+		l.add(40);
+		l.add(50);
+		l.add(60);
+		l.add(70);
+		
+		for(int i=0;i<l.size();i++)
+		{
+			if(l.get(i)>max)
+			{
+				max=l.get(i);
+			}
+		}
+		System.out.println(max);
+	}
+}

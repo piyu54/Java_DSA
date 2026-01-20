3bd648742523c96e5cc43ce9bea957b6fa7838b6
asus <priyakushwaha1407@gmail.com>
2026-01-20 17:21:33 +0530

    commit

---
+++ Prime.java
@@ -0,0 +1,40 @@
+
+public class Prime {
+
+	public static void main(String[] args) {
+		
+
+
+		int n=2;
+		boolean isprime=true;
+		
+		if(n<2)
+		{
+			isprime=false;
+		}
+		else
+		{
+			
+		}
+		
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				isprime=false;
+				break;
+			}
+		}
+		
+		if(isprime)
+		{
+			System.out.println("Prime number");
+		}
+		else
+		{
+			System.out.println("Not a Prime Number");
+		}
+		
+	
+	}
+}

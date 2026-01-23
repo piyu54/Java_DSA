b018679f1d61e751ba8124e9feaca4a5f8a31600
asus <priyakushwaha1407@gmail.com>
2026-01-23 23:42:48 +0530

    commit

--- src/com/loops/Whileloopdemo.java
+++ src/com/loops/Whileloopdemo.java
@@ -4,11 +4,11 @@ public class Whileloopdemo {
 
 	public static void main(String[] args) {
 		
-		int i=1;
-		while(i<=10)
+		int counter=1;
+		while(counter<=10)
 		{
-			System.out.println(i);
-			i++;
+			System.out.println(counter);
+			counter++;
 		}
 	}
 }

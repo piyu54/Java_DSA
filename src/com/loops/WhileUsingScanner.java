c35bcff8410fda0e0c1066df529ca3c7636f5918
asus <priyakushwaha1407@gmail.com>
2026-01-23 23:47:45 +0530

    commit

--- src/com/loops/WhileUsingScanner.java
+++ src/com/loops/WhileUsingScanner.java
@@ -1,8 +1,20 @@
 package com.loops;
 
+import java.util.Scanner;
+
 public class WhileUsingScanner {
 
 	public static void main(String[] args) {
 		
+		Scanner s = new Scanner(System.in);
+		int num=s.nextInt();
+		int i=1;
+		int sum=0;
+		while(i<=num)
+		{
+			sum+=i;
+			i++;
+		}
+		System.out.println(sum);
 	}
 }

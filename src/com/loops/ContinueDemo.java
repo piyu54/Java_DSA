ac05d475d2cb36075883311c897d1dc3bb261ff6
asus <priyakushwaha1407@gmail.com>
2026-01-24 23:04:30 +0530

    commit

---
+++ src/com/loops/ContinueDemo.java
@@ -0,0 +1,15 @@
+package com.loops;
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			if(i==3)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}

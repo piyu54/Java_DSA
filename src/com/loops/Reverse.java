6eb1ec47401cf861c8a55a1e0231886fe02d6735
asus <priyakushwaha1407@gmail.com>
2026-01-24 23:04:47 +0530

    commit

---
+++ src/com/loops/Reverse.java
@@ -0,0 +1,16 @@
+package com.loops;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		
+		int num=1407;
+		
+		while(num>0)
+		{
+			int lastdigit=num%10;
+			System.out.print(lastdigit);
+			num=num/10;
+		}
+	}
+}

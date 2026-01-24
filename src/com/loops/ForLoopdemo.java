140aba0c13ef1afa362effff7fb11f1a19a55994
asus <priyakushwaha1407@gmail.com>
2026-01-24 23:04:39 +0530

    commit

--- src/com/loops/ForLoopdemo.java
+++ src/com/loops/ForLoopdemo.java
@@ -2,4 +2,17 @@ package com.loops;
 
 public class ForLoopdemo {
 
+	public static void main(String[] args) {
+		
+//		for(int i=1;i<=10;i++)
+//		{
+//			System.out.println(i);
+//		}
+//		
+		for(int i=1;i<=4;i++)
+		{
+			System.out.println("****");
+		}
+
+	}
 }

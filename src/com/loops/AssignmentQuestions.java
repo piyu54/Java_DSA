6fa93207b266a63cbe2273ca87a1bd91182cea80
asus <priyakushwaha1407@gmail.com>
2026-01-25 21:08:01 +0530

    commit

---
+++ src/com/loops/AssignmentQuestions.java
@@ -0,0 +1,58 @@
+package com.loops;
+
+import java.util.Scanner;
+
+public class AssignmentQuestions {
+
+	public static void main(String[] args) {
+
+		// 2
+
+		Scanner s = new Scanner(System.in);
+//		System.out.println("Enter an Total num of inputs ");
+//		int N=s.nextInt();
+//		
+//		int evenNum=0;
+//		int oddNum=0;
+//		
+//		while(N>0)
+//		{
+//			int currNum=s.nextInt();
+//			if(currNum% 2==0)
+//			{
+//				evenNum+=currNum;
+//			}
+//			else
+//			{
+//				oddNum+=currNum;
+//			}
+//			N--;
+//		}
+//		System.out.println("Even num sum is:"+evenNum);
+//		System.out.println("Odd num sum is:"+oddNum);
+
+		// 3 Factorial
+
+//		int num=5;
+//		int fact = 1;
+//		
+//		for(int i=1;i<=num;i++)
+//		{
+//			fact=fact*i;
+//		}
+//		System.out.println(fact);
+
+		// 4 Multiplication table
+
+//		System.out.println("Enter an number");
+//		
+//		int num=s.nextInt();
+//		
+//		for(int i=1;i<=10;i++)
+//			
+//		{
+//			System.out.println(num+" * "+i+" = "+num*i);
+//		}
+
+	}
+}

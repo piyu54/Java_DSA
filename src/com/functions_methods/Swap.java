e7df1531c1fc216e4a7d1c5a324d4de6fab8ce77
asus <priyakushwaha1407@gmail.com>
2026-02-23 20:59:36 +0530

    commit

---
+++ src/com/functions_methods/Swap.java
@@ -0,0 +1,36 @@
+package com.functions_methods;
+
+public class Swap {
+
+	public static void swap()
+	{
+		int a=8;
+		int b=9;
+		int temp;
+		
+		System.out.println("Before swapping :a "+a);
+		System.out.println("Before swapping :b "+b);
+
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("after swapping :a "+a);
+		System.out.println("after swapping :b "+b);
+		
+	}
+	
+//	public static void changeA(int change)
+//	{
+//		change=99;
+//	}
+//	public static void main(String[] args) {
+//		
+//		swap();
+//		int change=5;
+//		changeA(change);
+//		System.out.println(change);
+		
+		
+//	}
+}

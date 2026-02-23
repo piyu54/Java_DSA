eaf53fb00c43b38e558a5652acde4cbb74c73a31
asus <priyakushwaha1407@gmail.com>
2026-02-23 20:59:25 +0530

    commit

---
+++ src/com/functions_methods/Prime.java
@@ -0,0 +1,29 @@
+package com.functions_methods;
+
+public class Prime {
+
+	
+	
+
+	
+	public static boolean isPrime(int n)
+	{
+		boolean isPrime=true;
+
+		for(int i=2;i<n-1;i++)
+		{
+			if(n%i==0)//com div
+			{
+				isPrime=false;
+				break;
+			}
+		}
+		return isPrime;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(isPrime(3));
+
+	}
+}

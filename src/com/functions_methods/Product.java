de3930f259c93509c8f7bb40ea496410e9fc92f2
asus <priyakushwaha1407@gmail.com>
2026-02-23 20:59:31 +0530

    commit

---
+++ src/com/functions_methods/Product.java
@@ -0,0 +1,21 @@
+package com.functions_methods;
+
+public class Product {
+
+	public static int multi(int a,int b)
+	{
+		int prod=a*b;
+		return prod;
+	}
+	
+	public static void main(String[] args) {
+		
+		int a=8;
+		int b=9;
+		
+		
+		System.out.println(multi(a, b));
+		
+		
+	}
+}

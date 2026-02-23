54350e64555c1b0e36aa8bbf74f764a278cf5163
asus <priyakushwaha1407@gmail.com>
2026-02-23 20:59:01 +0530

    commit

---
+++ src/com/functions_methods/Demo.java
@@ -0,0 +1,20 @@
+package com.functions_methods;
+
+public class Demo {
+
+	public static void helloWorld()
+	{
+		int id=99;
+		System.out.println("hello");
+		System.out.println("world");
+		System.out.println(id);
+		
+	}
+	public static void main(String[] args) {
+		
+		helloWorld();
+		helloWorld();
+		
+		
+	}
+}

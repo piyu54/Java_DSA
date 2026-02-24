ada301e917fd86b041923b2e205d4363435aca9b
asus <priyakushwaha1407@gmail.com>
2026-02-24 21:18:58 +0530

    commit

---
+++ src/com/arraylist/BubbleSort.java
@@ -0,0 +1,13 @@
+package com.arraylist;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter an 5 number");
+				
+	}
+}

610936e76f681facaf0c20d1d3f74e42cf14c01a
asus <priyakushwaha1407@gmail.com>
2026-03-05 20:52:54 +0530

    commit

--- src/com/examples/Sumof2num.java
+++ src/com/examples/Sumof2num.java
@@ -2,7 +2,7 @@ package com.examples;
 
 public class Sumof2num {
 	public static void main(String[] args) {
-		//sum of two num
+		//sum of two number
 		int a = 10;
 		int b = 20;
 		int sum = a + b;
